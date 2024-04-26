7721bde6ec8f2ddeb6fe686d1e7a0ec119013a70
Your Name <you@example.com>
2024-04-26 16:56:53 +0200

    testing

--- test/wdio/electron.test.js
+++ test/wdio/electron.test.js
@@ -1,4 +1,4 @@
-import { browser } from '@wdio/globals'
+import { $,browser } from '@wdio/globals'
 import fs from 'node:fs'
 import path from 'node:path'
 
@@ -8,15 +8,35 @@ const { name, version } = packageJson
 describe('Electron Testing', () => {
     it('should print and verify the application title ', async () => {
         let b =await browser.getTitle();
-        console.log('Salut ', b)
+        // console.log('Salut ', b)
         expect(b).toEqual("Haino")
     })
 
-    it('should retrieve app metadata through the electron API', async () => {
-        const appName = await browser.electron.app('getName')
-        expect(appName).toEqual(name)
-        const appVersion = await browser.electron.app('getVersion')
-        expect(appVersion).toEqual(version)
-      })
+    it('should write to the commandInput and retrieve info message', async () => {
+        await browser.waitUntil(async () => {
+            const readyState = await browser.execute(() => document.readyState);
+            return readyState === 'complete';
+        });
+
+        // Ensuite, écrire "/hpm" dans le champ d'entrée avec l'ID "commandInput"
+        await browser.execute(() => {
+            const commandInput = document.getElementById('commandInput');
+            commandInput.value = "/hpm";
+            const event = new Event('keyup', { bubbles: true });
+            commandInput.dispatchEvent(event);
+        });
+
+        await browser.pause(1000);
+
+
+        // Retrieve the content of the paragraph with id "info"
+        const infoMessage = await browser.execute(() => {
+            return document.getElementById('info').innerText;
+        });
+        console.log("INFOMESSAGE", infoMessage)
+
+        // Assert that the retrieved message matches the expected result
+        expect(infoMessage).toContain('Searching');
+    });
 })
 
